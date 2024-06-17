7fdb29b972359c79d5c68377e3f9bd7d26217534
SrLillo <jlmlillo@hotmail.com>
2024-06-17 16:36:51 +0200

    Tratamiento de errores 404 y 400 - Personalización de mensajes

--- src/main/java/com/foro_hub/api/topico/TopicoDTO.java
+++ src/main/java/com/foro_hub/api/topico/TopicoDTO.java
@@ -6,17 +6,18 @@ import jakarta.validation.constraints.NotNull;
 
 public record TopicoDTO(
         Long id,
-        @NotBlank
+        @NotBlank(message = "Título es obligatorio")
         String titulo,
-        @NotBlank
+        @NotBlank(message = "Mensaje es obligatorio")
         String mensaje,
-        @NotNull
-        @JsonAlias("fecha_creacion") String fechaCreacion,
-        @NotNull
+        @NotNull(message = "Fecha de Creación es obligatorio")
+        @JsonAlias("fecha_creacion")
+        String fechaCreacion,
+        @NotNull(message = "Status es obligatorio")
         int status,
-        @NotBlank
+        @NotBlank(message = "Autor es obligatorio")
         String autor,
-        @NotBlank
+        @NotBlank(message = "Curso es obligatorio")
         String curso,
         String respuestas) {
 }
