58e8292be4666b339bcf232a95e04129350e29ac
SrLillo <jlmlillo@hotmail.com>
2024-06-21 15:28:05 +0200

    Agregado anotación @Repository en repositorios

--- src/main/java/com/foro_hub/api/domain/topico/TopicoRepository.java
+++ src/main/java/com/foro_hub/api/domain/topico/TopicoRepository.java
@@ -2,9 +2,11 @@ package com.foro_hub.api.domain.topico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
     @Query("SELECT t FROM Topico t ORDER BY FUNCTION('STR_TO_DATE', t.fechaCreacion, '%m/%d/%Y') ASC")
     List<Topico> findAllByFechaCreacionAsc();
--- src/main/java/com/foro_hub/api/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/foro_hub/api/domain/usuario/UsuarioRepository.java
@@ -2,7 +2,9 @@ package com.foro_hub.api.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     UserDetails findByUsuario(String username);
 }
